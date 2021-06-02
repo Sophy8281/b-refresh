4ec6a3013e04864b5b92aa824b9dc97ba2944a99
sophy8281 <wanjikusophia3@gmail.com>
2021-06-02 16:33:01 +0300

    first commit

---
+++ casting.py
@@ -0,0 +1,23 @@
+# Integers
+a = int(1)  # a will be 1
+b = int(2.8)  # b will be 2
+c = int("3")  # c will be 3
+print(a)
+print(b)
+print(c)
+# Floats
+d = float(1)  # d will be 1.0
+e = float(2.8)  # e will be 2.8
+f = float("3")  # f will be 3.0
+g = float("4.2")  # g will be 4.2
+print(d)
+print(e)
+print(f)
+print(g)
+# Strings
+h = str("s1")  # h will be 's1'
+i = str(2)  # i will be '2'
+j = str(3.0)  # j will be '3.0'
+print(h)
+print(i)
+print(j)
