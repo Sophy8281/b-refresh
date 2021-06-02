c59918c002ef5feef9247f8f3b1ac5b6c32ee32d
sophy8281 <wanjikusophia3@gmail.com>
2021-06-02 16:07:09 +0300

    first commit

---
+++ variables.py
@@ -0,0 +1,21 @@
+# Variables
+a = 5
+b = "Variable"
+print(a)
+print(b)
+
+c = 4  # x is of type int
+c = "Sally"  # x is now of type str
+print(c)
+
+d = "awesome"
+print("Python is " + d)
+
+e = "Python is "
+f = "awesome"
+g = e + f
+print(g)
+
+h = 5
+i = 10
+print(h + i)
